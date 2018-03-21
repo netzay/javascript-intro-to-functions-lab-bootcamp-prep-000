799e40207bc926f0333476d57fdf19158aa9a558
Ally Netzel <netzelally@gmail.com>
2018-03-21 02:01:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ function logWhisper(string){
 }
 function sayHiToGrandma(string){
   var lowercase = 'hello!'
-  if (string.toLowerCase() === lowercase)
+  if (lowercase.toLowerCase() === lowercase)
   return "I can't hear you!"
 }
 function sayHiToGrandma(string){
   var uppercase = 'HELLO'
-  if (string.toUpperCase() === UPPERCASE)
+  if (string.toUpperCase() === uppercase)
   return "YES INDEED!"
 }
 function sayHiToGrandma(string){
