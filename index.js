b69790728eb58435042fad0495b571617ed99772
Ally Netzel <netzelally@gmail.com>
2018-03-21 02:02:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function logWhisper(string){
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string){
-  var lowercase = 'hello!'
+  var lowercase = string
   if (lowercase.toLowerCase() === lowercase)
   return "I can't hear you!"
 }
