7562461df0b29bda19f902df24463fb6fea70d23
Ally Netzel <netzelally@gmail.com>
2018-03-21 02:06:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,18 +11,4 @@ function logWhisper(string){
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string){
-  var lowercase = string
-  if (lowercase.toLowerCase() === lowercase)
-  return "I can't hear you!"
-}
-function sayHiToGrandma(string){
-  var uppercase = string
-  if (string.toUpperCase() === uppercase)
-  return "YES INDEED!"
-}
-function sayHiToGrandma(string){
-  var mixedCase = string
-  if (mixedCase.toLowerCase() === mixedCase)
-  if (mixedCase.toUpperCase() === mixedCase)
-  return "I love you, too."
-}
+  if `string`.toUpperCase()rCase
